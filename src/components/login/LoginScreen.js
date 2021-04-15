e7cd39e8362ab032d75cea0c451863888dabb25a
JuanEscurra <archys.wow@gmail.com>
2021-04-15 18:54:01 -0500

    1.0.1

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -18,19 +18,20 @@ export const LoginScreen = ({history}) => {
 
     return (
         <div className="container">
-                <div className="vh-100 d-flex align-items-center justify-content-center">
-                    <form onSubmit={handleSubmit} className="w-25">
-                        <div className="mb-3">
-                            <label className="form-label">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" />
-                        </div>
-                        <button type="submit" className="btn btn-primary w-100">Ingrese</button>
-                    </form>
-                </div>
+            <div className="vh-100 d-flex flex-column align-items-center justify-content-center">
+                <h1 className="my-5">Heroes APP</h1>
+                <form onSubmit={handleSubmit} className="w-25" style={{minWidth:300}}>
+                    <div className="mb-3">
+                        <label className="form-label">Email address</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <button type="submit" className="btn btn-primary w-100">Ingrese</button>
+                </form>
+            </div>
         </div>
     )
 }
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,22 +2,29 @@ import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
-    Route
+    Route,
+    Redirect
   } from "react-router-dom";
 import { AuthContext } from '../Auth/AuthContext';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
+
 import { LoginScreen} from '../components/login/LoginScreen'
 import { DashboardRouter } from './DashboardRouter';
-import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
     const {user} = useContext(AuthContext);
-    console.log(user);
+
     return (
         <Router>
             <div>
                 <Switch>
                     
-                    <Route  path="/login" component={LoginScreen}/>
+                    <PublicRoute
+                        path="/login"
+                        component={LoginScreen}
+                        isAuthenticated={user.logged}
+                    />
                     <PrivateRoute 
                         path="/" 
                         component={ DashboardRouter } 
